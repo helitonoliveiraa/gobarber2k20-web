d5e5790af12b2e7de0396187515950678a6ea582
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-01-21 14:24:29 -0300

    :sparkles: feat: forgot password

---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,114 @@
+import React, { useCallback, useRef, useState } from 'react';
+import { FiArrowLeft, FiMail } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+
+import getValidationErrors from '../../utils/validationErros';
+
+// import { useAuth } from '../../context/hooks/Auth';
+import { useToast } from '../../context/hooks/Toast';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  // eslint-disable-next-line indent
+  Container,
+  Content,
+  AnimationContainer,
+  Background,
+} from './styles';
+import api from '../../services/api';
+
+interface ForgotPasswordFormData {
+  email: string;
+}
+
+const ForgotPassword: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+
+  const handleLogin = useCallback(
+    async (data: ForgotPasswordFormData) => {
+      try {
+        setLoading(true);
+
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('Digite um e-mail válido')
+            .required('E-mail é obrigatório'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        // recuperação de senha
+        await api.post('/password/forgot', {
+          email: data.email,
+        });
+
+        addToast({
+          type: 'success',
+          title: 'E-mail de recuperação enviado.',
+          description:
+            'Enviamos um e-mail para confirmar a recuperação da senha, cheque sua caixa de entrada.',
+        });
+      } catch (err) {
+        console.log({ err });
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação!',
+          description:
+            'Ocorreu um erro ao tentar realizar a reduperação de senha, tente novamente.',
+        });
+      } finally {
+        setLoading(false);
+      }
+    },
+    [addToast],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleLogin}>
+            <h1>Recuperar senha</h1>
+
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+            <Button loading={loading} type="submit">
+              Recuperar
+            </Button>
+          </Form>
+
+          <Link to="/">
+            <FiArrowLeft size={20} />
+            Voltar ao login
+          </Link>
+        </AnimationContainer>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
+
+export default ForgotPassword;
