148c4d7c72207b88f2b9db25108e40a8d8b12412
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-01-27 16:04:26 -0300

    :recycle: refactory: code refactory

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -10,7 +10,6 @@ interface TooltipProp {
 const Tooltip: React.FC<TooltipProp> = ({ title, className, children }) => (
   <Container className={className} data-title={title}>
     {children}
-    {/* <span>{title}</span> */}
   </Container>
 );
 
