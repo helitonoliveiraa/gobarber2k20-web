ebf8426f093848239f2daefe3311379518a4f29b
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-01-28 12:48:26 -0300

    :sparkles: feat: created function for that to be possible to update user profile

--- src/context/hooks/Auth.tsx
+++ src/context/hooks/Auth.tsx
@@ -29,6 +29,7 @@ interface AuthContextData {
   user: User;
   signIn(credentials: SignInCredenctial): Promise<void>;
   signOut(): void;
+  updateUser(user: User): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -72,8 +73,22 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
