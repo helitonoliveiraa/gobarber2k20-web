aeb6777b54c662f35a873d956abfd7c625f68b35
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-02-18 11:53:01 -0300

    :rocket: chore: loading .env files to deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+console.log(process.env.REACT_APP_API_URL);
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
