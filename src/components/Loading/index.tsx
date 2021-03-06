1a778bd963f770fbc234417523d9c1ba7a5a190c
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-03-06 16:16:45 -0300

    :sparkles: feat: loader component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
+const Loading: React.FC = () => (
+  <div style={{ alignSelf: 'center' }}>
+    <Loader type="Puff" color="#FF9000" height={50} width={50} timeout={0} />
+  </div>
+);
+
+export default Loading;
